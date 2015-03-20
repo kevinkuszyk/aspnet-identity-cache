2139adff0f02be98bbc38962b175c8fbf4312206
Kevin Kuszyk <kevin@kevinkuszyk.com>
2015-03-20 14:07:42 +0000

    Add user controller to web app 2

---
+++ src/WebApplication2/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace WebApplication2.Controllers
+{
+    public class UserController : Controller
+    {
+        private ApplicationUserManager _userManager;
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        public ActionResult Index()
+        {
+            return View(UserManager.Users);
+        }
+    }
+}
