a8651e5c2d624f5128911bfa8a6425a454811b58
Kevin Kuszyk <kevin@kevinkuszyk.com>
2015-03-20 14:04:07 +0000

    Add initial migration in web app 2

---
+++ src/WebApplication2/Migrations/Configuration.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using WebApplication2.Models;
+
+namespace WebApplication2.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<WebApplication2.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(WebApplication2.Models.ApplicationDbContext context)
+        {
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            if (userManager.FindByName("foo") == null)
+            {
+                userManager.Create(new ApplicationUser { UserName = "bar", Name = "Bar User" });
+            }
+        }
+    }
+}
